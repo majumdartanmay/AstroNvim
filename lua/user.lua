4e0fc7ad518a7f6853c174f345daf11b6337c2eb
Tanmay Majumdar <tanmaymajumdar5612@gmail.com>
2023-12-23 16:03:21 +0530

    Add key mappings to delete without pasting.

--- lua/user.lua
+++ lua/user.lua
@@ -4,20 +4,29 @@ local utils = require "astronvim.utils"
 
 local maps = require("astronvim.utils").empty_map_table()
 maps.n["<leader>a"] = { "ggvG", desc = "Select all" }
--- maps.v["//gf"] = { function()
---     vim.lsp.buf.format({ async = true })
--- end, {}}
---
 
-function FormatFunction()
-  vim.lsp.buf.format({
-    async = true,
-    range = {
-      ["start"] = vim.api.nvim_buf_get_mark(0, "<"),
-      ["end"] = vim.api.nvim_buf_get_mark(0, ">"),
-    }
-  })
+local range_formatting = function()
+    local start_row, _ = unpack(vim.api.nvim_buf_get_mark(0, "<"))
+    local end_row, _ = unpack(vim.api.nvim_buf_get_mark(0, ">"))
+    vim.lsp.buf.format({
+        range = {
+            ["start"] = { start_row, 0 },
+            ["end"] = { end_row, 0 },
+        },
+        async = false,
+    })
 end
 
-vim.api.nvim_set_keymap("v", "\\qf", "<Esc><cmd>lua FormatFunction()<CR>", {noremap = true})
+vim.keymap.set("v", "<leader>123", range_formatting, { desc = "Range Formatting" })
+vim.keymap.set("n", "dd", "\"_dd", { desc = "Remove line without cutting "})
+vim.keymap.set("n", "dw", "\"_dw", { desc = "Remove word without cutting "})
+vim.keymap.set("n", "diw", "\"_diw", { desc = "Remove WORD without cutting "})
+vim.keymap.set("n", "x", "\"_x", { desc = "Remove character without cutting "})
+
+vim.keymap.set("n", "<leader>dd", "dd", { desc = "Remove line with cutting "})
+vim.keymap.set("n", "<leader>dw", "dw", { desc = "Remove word with cutting"})
+vim.keymap.set("n", "<leader>x", "x", { desc = "Remove character with cutting "})
+vim.keymap.set("n", "<leader>diw", "diw", { desc = "Remove WORD with cutting " })
+-- Range formatting
+
 utils.set_mappings(astronvim.user_opts("mappings", maps))
